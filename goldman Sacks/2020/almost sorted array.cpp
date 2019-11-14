776f2e2cee8870c9dcaeb2ef46cf2355ecabdec2
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-11-15 00:49:59 +0530

    Create almost sorted array.cpp

---
+++ goldman Sacks/2020/almost sorted array.cpp
@@ -0,0 +1,28 @@
+/**Almost sorted array*/
+
+int Solution::lis(const vector<int> &A) 
+{
+   vector<int> ans;
+   int len=-1;
+   if(A.size()==0)return 0;
+   if(A.size()==1)return 1;
+   ans.push_back(A[0]);
+   len++;
+   for(int i=1;i<A.size();i++)
+   {
+       if(A[i]<ans[0])ans[0]=A[i];
+       else if(ans[len]<A[i])
+       {
+           ans.push_back(A[i]);
+           len++;
+       }
+       else
+       {
+           auto x=lower_bound(ans.begin(), ans.end(), A[i]);
+           ans[x-ans.begin()]=A[i];
+       }
+   }
+   len++;
+   if(len==0||len==1)return 0;
+   return len-1;
+}
